6df3a0f7d5a647b921c03b277f98428b677c3c2b
Fonfond <141838838+Fonfond@users.noreply.github.com>
2026-08-27 12:44:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python manage.py migrate
-gunicorn navoke_backend.wsgi --bind 0.0.0.0:8000 --log-file -
+gunicorn navoke_backend.wsgi --bind 0.0.0.0:8000 --timeout 120 --workers 2 --preload --log-file -
