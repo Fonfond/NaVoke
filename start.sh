5ca1461bb495649a68aa1e4b39e67ccabef7ab00
Fonfond <141838838+Fonfond@users.noreply.github.com>
2026-08-28 17:45:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,8 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 gunicorn navoke_backend.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --preload
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin492357816')
