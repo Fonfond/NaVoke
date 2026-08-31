dbed3a1f5fbecd19a0d8664c301681a734b6be6c
Fonfond <141838838+Fonfond@users.noreply.github.com>
2026-08-31 12:38:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,15 +6,16 @@ python manage.py migrate --noinput
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
+# Ищем пользователя admin
 user = User.objects.filter(username='admin').first()
 if user:
     user.is_staff = True
     user.is_superuser = True
+    user.role = 'admin'  # ✅ Меняем роль в приложении
     user.is_active = True
     user.save()
     print('Пользователь admin теперь администратор!')
 else:
-    print('Пользователь admin не найден, создаю...')
     User.objects.create_superuser('admin', 'admin@example.com', 'admin492357816')
 "
 
