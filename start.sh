89ce0058d41cc0b6a75b6facced1ab20de5474ca
Fonfond <141838838+Fonfond@users.noreply.github.com>
2026-08-31 12:12:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,15 @@
 set -e
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn navoke_backend.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --preload
+
+# Создаём суперпользователя ПЕРЕД запуском Gunicorn
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin492357816')
+print('Суперпользователь создан!')
+"
+
+# Запускаем Gunicorn
+exec gunicorn navoke_backend.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --preload
